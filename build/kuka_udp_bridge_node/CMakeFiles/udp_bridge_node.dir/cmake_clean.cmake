237ff358feb99611b27dd48e039b98e481ac5f2b
Lufs Seccus <congthanhvu1208@gmail.com>
2026-07-17 17:34:34 +0200

    Initial commit

---
+++ build/kuka_udp_bridge_node/CMakeFiles/udp_bridge_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/udp_bridge_node.dir/src/udp_bridge_node.cpp.o"
+  "CMakeFiles/udp_bridge_node.dir/src/udp_bridge_node.cpp.o.d"
+  "udp_bridge_node"
+  "udp_bridge_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/udp_bridge_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
